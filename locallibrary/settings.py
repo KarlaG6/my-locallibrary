967566e2f6300446d5493c8545dfa84640cd8862
karlaG6 <karlaisabelgt@gmail.com>
2021-09-14 00:26:52 -0500

    Some settings and the gitignore

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xr$pn7jki63=g1=bs78um4px%2-z(si12%u#lt@cj47zgrbu3e'
+# SECRET_KEY = 'django-insecure-xr$pn7jki63=g1=bs78um4px%2-z(si12%u#lt@cj47zgrbu3e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-xr$pn7jki63=g1=bs78um4px%2-z(si12%u#lt@cj47zgrbu3e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +133,5 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
